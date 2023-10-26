9a3ff61abe4bd5fcdb370549988a3f3bc95b4218
Mounir <lm_melzi@esi.dz>
2023-10-26 21:25:56 +0200

    mobile responsive text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex h-full items-center justify-center text-9xl font-light">
+    <div className="flex h-full items-center justify-center text-6xl md:text-9xl font-light">
       VALORESI
     </div>
   );
