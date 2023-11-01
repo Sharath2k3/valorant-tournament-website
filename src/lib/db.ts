20112cbdd6fa61d191d1dfe517bf5079b3bf2450
Mounir <lm_melzi@esi.dz>
2023-11-01 13:24:59 +0100

    connect to mongodb

---
+++ src/lib/db.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export async function connectDB(): Promise<void> {
+  if (isConnected) {
+    console.log("Already connected to mongodb");
+    return;
+  }
+
+  if (!process.env.DATABASE_URL) {
+    console.log("Database connection URL not found");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    isConnected = true;
+    console.log("Connected to database");
+  } catch (e: any) {
+    console.log(e);
+  }
+}
