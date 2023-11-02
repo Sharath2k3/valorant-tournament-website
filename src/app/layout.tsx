d4db5ca291f1bb14c839c1ca9d85b8f70828a030
Mounir <lm_melzi@esi.dz>
2023-11-02 18:58:41 +0100

    install shadcn Toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/toaster";
+
 const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={font.className} suppressHydrationWarning>
-        {children}
+        <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   );
